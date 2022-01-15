af20dda36c3258ba5f9b21c8dc38544cfcdb3940
Nader Nasr <nadernasr7@gmail.com>
2022-01-15 08:46:20 -0500

    sum of two numbers using recursion

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+const sum = (fromN, toN) => {
+  const sameNumber = fromN - toN;
+  let sumOf = 0;
+  if (sameNumber === 0) {
+    return fromN;
+  }
+  if (fromN <= toN) {
+
+    sumOf = fromN + sum(fromN + 1, toN);
+    return sumOf;
+  }
+
+};
 
 module.exports = sum;
